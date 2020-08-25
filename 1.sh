42e75a8006c4f000624d9b59181f36e8adf52596
n0rvik <61582087+n0rvik@users.noreply.github.com>
2020-08-25 11:05:55 +0300

    Change 1.sh

--- 1.sh
+++ 1.sh
@@ -7,8 +7,9 @@
 cp -v ./.bash_aliases $HOME/.bash_aliases
 cp -v ./.bash_profile $HOME/.bash_profile
 cp -v ./.bashrc $HOME/.bashrc
+cp -v ./.bashrc $HOME/.bash_logout
 cp -v ./.dir_colors $HOME/.dir_colors
 
 if [ ! "x$1" == "x" ]; then
-  chown $1:$1 $HOME/.bash_aliases $HOME/.bash_profile $HOME/.bashrc $HOME/.dir_colors
+  chown $1:$1 $HOME/.bash_aliases $HOME/.bash_profile $HOME/.bashrc $HOME/.dir_colors $HOME/.bash_logout
 fi
