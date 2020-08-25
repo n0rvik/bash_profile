69184e8cf286c3d0cb98be67675dcaa57e3f8a37
n0rvik <61582087+n0rvik@users.noreply.github.com>
2020-08-25 11:02:10 +0300

    Change 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#
+# Копирует файлы настроек bash в личную папку пользователя
+#
+
 cp -v ./.bash_aliases $HOME/.bash_aliases
 cp -v ./.bash_profile $HOME/.bash_profile
 cp -v ./.bashrc $HOME/.bashrc
+cp -v ./.dir_colors $HOME/.dir_colors
+
+if [ ! "x$1" == "x" ]; then
+  chown $1:$1 $HOME/.bash_aliases $HOME/.bash_profile $HOME/.bashrc $HOME/.dir_colors
+fi
