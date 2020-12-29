fafb3aa38a0366024a21dc660b4118df99268c51
n0rvik <61582087+n0rvik@users.noreply.github.com>
2020-12-29 17:55:52 +0300

    Change aide1.sh

--- aide/aide1.sh
+++ aide/aide1.sh
@@ -13,10 +13,15 @@ MAIL_ADDR=root@${HOSTNAME}
 AIDE=$(which aide)
 NICE=$(which nice)
 RM=$(which rm)
+CP=$(which cp)
+CAT=$(which cat)
+MAIL=$(which mail)
+DOTLOCKFILE=$(which dotlockfile)
+MKTEMP=$(which mktemp)
 
-dotlockfile -p ${LOCK_FILE} || exit 1
+${DOTLOCKFILE} -p ${LOCK_FILE} || exit 1
 
-TMP=$(mktemp -t aide.XXXXXX)
+TMP=$(${MKTEMP} -t aide.XXXXXX)
 if [ $? -ne 0 ]; then
   echo "Can't create tmp file."
   exit 1
@@ -28,16 +33,16 @@ ${NICE} -n 19 ${AIDE} --update >${TMP} 2>&1
 ret=$?
 if [ ${ret} -eq 0 ]; then
   # Nothing is changed.
-  cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
+  ${CP} /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
 elif [ ${ret} -lt 8 ]; then
   # Some file is changed.
-  cat ${TMP} | mail -s "AIDE detects changes. Host: ${HOSTNAME}" ${MAIL_ADDR}
-  cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
+  ${CAT} ${TMP} | ${MAIL} -s "AIDE detects changes. Host: ${HOSTNAME}" ${MAIL_ADDR}
+  ${CP} /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
 else
   # Cannot update database.
-  cat ${TMP} | mail -s "AIDE fatal error. Host: ${HOSTNAME}" ${MAIL_ADDR}
+  ${CAT} ${TMP} | ${MAIL} -s "AIDE fatal error. Host: ${HOSTNAME}" ${MAIL_ADDR}
 fi
 
-dotlockfile -u ${LOCK_FILE}
+${DOTLOCKFILE} -u ${LOCK_FILE}
 
 
