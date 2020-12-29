ad7b6fca353f609bddb39608acbaa42b6d55bf20
n0rvik <61582087+n0rvik@users.noreply.github.com>
2020-12-29 15:32:13 +0300

    App cp2orig.sh

--- cp2home.sh
+++ cp2home.sh
@@ -19,7 +19,9 @@ mkdir -vp $_home/.config/bashrc.d
 
 cp -v ./.bash_aliases $_home/.bash_aliases
 cp -v ./.bash_profile $_home/.bash_profile
+cp -v ./.bash_profile.orig $_home/.bash_profile.orig
 cp -v ./.bashrc       $_home/.bashrc
+cp -v ./.bashrc.orig  $_home/.bashrc.orig
 cp -v ./.bash_logout  $_home/.bash_logout
 cp -v ./.dir_colors   $_home/.dir_colors
 cp -v ./.inputrc      $_home/.inputrc
@@ -27,7 +29,9 @@ cp -v ./.inputrc      $_home/.inputrc
 if [ ! "x$1" == "x" ]; then
   chown -v $1:$1 $_home/.bash_aliases \
                  $_home/.bash_profile \
+                 $_home/.bash_profile.orig \
                  $_home/.bashrc \
+                 $_home/.bashrc.orig \
                  $_home/.dir_colors \
                  $_home/.bash_logout \
                  $_home/.inputrc
---
+++ cp2orig.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# Восстанавливаем старые настройки bash
+#
+
+_home=$HOME
+if [ ! "x$1" == "x" ]; then
+   if [ -d "/home/$1" ]; then
+     _home=/home/$1
+   else
+     echo "Directory not exist. Stop."
+     exit 1
+   fi
+fi
+
+
+if [ -f $_home/.bashrc.new ]; then
+  echo 'Restore new file'
+  mv -vi $_home/.bash_aliases.new $_home/.bash_aliases
+  mv -vi $_home/.bash_profile.new $_home/.bash_profile
+  mv -vi $_home/.bashrc.new       $_home/.bashrc
+else 
+  echo 'Restore origin file.'
+  cp -vi $_home/.bash_aliases $_home/.bash_aliases.new
+  cp -vi $_home/.bash_profile $_home/.bash_profile.new
+  cp -vi $_home/.bashrc       $_home/.bashrc.new
+
+  cp -vi $_home/.bash_profile.orig $_home/.bash_profile
+  cp -vi $_home/.bashrc.orig  $_home/.bashrc
+fi
+exit 0
