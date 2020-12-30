409a23ceb0482f420cf042d6ae93f3b6e5d2037c
n0rvik <61582087+n0rvik@users.noreply.github.com>
2020-12-30 14:53:54 +0300

    Change cp2home.sh

--- cp2home.sh
+++ cp2home.sh
@@ -4,6 +4,8 @@
 # Копирует файлы настроек bash в личную папку пользователя
 #
 
+set -u
+
 _home=$HOME
 if [ ! "x$1" == "x" ]; then
    if [ -d "/home/$1" ]; then
@@ -14,24 +16,33 @@ if [ ! "x$1" == "x" ]; then
    fi
 fi
 
+mkdir -vp $_home/.config/backup
 mkdir -vp $_home/.config/bashrc
 mkdir -vp $_home/.config/bashrc.d
 
+if [ ! "x$1" == "x" ]; then
+  chown -Rv $1:$1 $_home/.config
+fi
+
 sfile=(.bash_aliases .bash_profile .bashrc .bash_logout .dir_colors .inputrc)
 
+star=
 for i in ${sfile[*]} ; do
-  if [ -f "$_home/${i}.orig" ]; then
-    /bin/cp -v $_home/${i}.orig $_home/${i}.bak
-  fi
-  if [ -f "$_home/${i}" ]; then
-    /bin/cp -v $_home/${i} $_home/${i}.orig
-  fi
+  star="${star} $_home/${i}"
+done
+
+/bin/tar -cvf $_home/.config/backup/.profile.`date +'%Y%m%d-%H%M%S'`.tar ${star}
+if [ ! "x$1" == "x" ]; then
+  chown -Rv $1:$1 $_home/.config/backup
+fi
+
+for i in ${sfile[*]} ; do
   /bin/cp -v ./${i} $_home/${i}
   if [ ! "x$1" == "x" ]; then
     chown -v $1:$1 $_home/${i}
-    chown -v $1:$1 $_home/${i}.orig
-    chown -v $1:$1 $_home/${i}.bak
   fi
 done
+
+
 
 exit 0
