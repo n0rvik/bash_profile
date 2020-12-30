25bebc3e99ad12d53476be977ec2c89218975688
n0rvik <61582087+n0rvik@users.noreply.github.com>
2020-12-30 14:28:04 +0300

    Change cp2home.sh

--- cp2home.sh
+++ cp2home.sh
@@ -17,24 +17,21 @@ fi
 mkdir -vp $_home/.config/bashrc
 mkdir -vp $_home/.config/bashrc.d
 
-cp -v ./.bash_aliases $_home/.bash_aliases
-cp -v ./.bash_profile $_home/.bash_profile
-cp -v ./.bash_profile.orig $_home/.bash_profile.orig
-cp -v ./.bashrc       $_home/.bashrc
-cp -v ./.bashrc.orig  $_home/.bashrc.orig
-cp -v ./.bash_logout  $_home/.bash_logout
-cp -v ./.dir_colors   $_home/.dir_colors
-cp -v ./.inputrc      $_home/.inputrc
+sfile=(.bash_aliases .bash_profile .bashrc .bash_logout .dir_colors .inputrc)
 
-if [ ! "x$1" == "x" ]; then
-  chown -v $1:$1 $_home/.bash_aliases \
-                 $_home/.bash_profile \
-                 $_home/.bash_profile.orig \
-                 $_home/.bashrc \
-                 $_home/.bashrc.orig \
-                 $_home/.dir_colors \
-                 $_home/.bash_logout \
-                 $_home/.inputrc
-fi
+for i in ${sfile[*]}; do
+  if [ -f "$_home/${i}.orig"]; then
+    /bin/cp -v $_home/${i}.orig $_home/${i}.bak
+  fi
+  if [ -f "$_home/${i}"]; then
+    /bin/cp -v $_home/${i} $_home/${i}.orig
+  fi
+  /bin/cp -v ./${i} $_home/${i}
+  if [ ! "x$1" == "x" ]; then
+    chown -v $1:$1 $_home/${i}
+    chown -v $1:$1 $_home/${i}.orig
+    chown -v $1:$1 $_home/${i}.bak
+  fi
+done
 
 exit 0
