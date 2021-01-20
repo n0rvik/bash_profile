b5298dd56e8516f0c50fbc301a9f537c770ad1e6
n0rvik <61582087+n0rvik@users.noreply.github.com>
2021-01-20 10:56:00 +0300

    Change .bash_small

--- bash_origin/cp2skel.sh
+++ bash_origin/cp2skel.sh
@@ -6,7 +6,7 @@
 
 _toskel=/etc/skel
 
-cp -v ./.bash_aliases $_toskel/.bash_aliases
+#cp -v ./.bash_aliases $_toskel/.bash_aliases
 cp -v ./.bash_profile $_toskel/.bash_profile
 cp -v ./.bashrc       $_toskel/.bashrc
 cp -v ./.bash_logout  $_toskel/.bash_logout
