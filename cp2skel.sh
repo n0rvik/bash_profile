7d195f2861b2708a07a534cc6b18eedf1c5f8608
n0rvik <61582087+n0rvik@users.noreply.github.com>
2021-01-20 10:25:21 +0300

    Change .bash_profile

--- cp2skel.sh
+++ cp2skel.sh
@@ -4,10 +4,10 @@
 # Копирует файлы настроек bash в личную папку skel
 #
 
-_home=/etc/skel
+_toskel=/etc/skel
 
-cp -v ./.bash_aliases $_home/.bash_aliases
-cp -v ./.bash_profile $_home/.bash_profile
-cp -v ./.bashrc       $_home/.bashrc
-cp -v ./.bash_logout  $_home/.bash_logout
+cp -v ./.bash_aliases $_toskel/.bash_aliases
+cp -v ./.bash_profile $_toskel/.bash_profile
+cp -v ./.bashrc       $_toskel/.bashrc
+cp -v ./.bash_logout  $_toskel/.bash_logout
 
