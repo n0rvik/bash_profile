ad5cad99ff744f3c83cecc9d16fc1923b42b8396
n0rvik <61582087+n0rvik@users.noreply.github.com>
2021-01-20 10:25:46 +0300

    Change .bash_small

---
+++ bash_small/cp2skel.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#
+# Копирует файлы настроек bash в личную папку skel
+#
+
+_toskel=/etc/skel
+
+cp -v ./.bash_aliases $_toskel/.bash_aliases
+cp -v ./.bash_profile $_toskel/.bash_profile
+cp -v ./.bashrc       $_toskel/.bashrc
+
