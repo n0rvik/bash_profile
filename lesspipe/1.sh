befcc2b710b2b5b60e2b38b5cff4b9cf4553a22e
n0rvik <61582087+n0rvik@users.noreply.github.com>
2021-09-07 11:50:03 +0300

    append change script install lesspipe

--- lesspipe/1.sh
+++ lesspipe/1.sh
@@ -6,7 +6,7 @@ set -e
 _LESSPIPE=lesspipe-1.89
 
 tar -xvf ${_LESSPIPE}.tar.gz
-cd ${_LESSPIPE} && ./install.sh 
+cd ${_LESSPIPE} && make && make install
 cd ..
 [ -d ${_LESSPIPE} ] && rm -rf ${_LESSPIPE}/
 
