9a722f77a66919554fb136ac71db2d93afe62796
n0rvik <61582087+n0rvik@users.noreply.github.com>
2021-09-09 17:36:50 +0300

    append change script install lesspipe

--- mc/1.sh
+++ mc/1.sh
@@ -6,3 +6,4 @@ cp bashrc ~/.local/share/mc/bashrc
 cat mc.menu | tee -a /etc/mc/mc.menu
 
 sed -i -e 's/skin=.*/skin=xoria256/i' ~/.config/mc/ini
+sed -i -e 's/editbold=.*/editbold=yellow;green;bold/i' /usr/share/mc/skins/xoria256.ini
